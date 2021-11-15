53ab92b24c83d36f2db73140e11c3c2306c8875d
Denise Maulidita <deemvi25@gmail.com>
2021-11-15 15:01:38 +0700

    add file index & latihan

---
+++ latihan_3_1.py
@@ -0,0 +1,31 @@
+# slide 7
+# print("Menghitung Luas Segitiga")
+# alas  = input("Alas :")
+# tinggi =  input("Tinggi : ")
+
+
+# hitung  = int(alas)*int(tinggi)
+# bagi = int(hitung)/2
+# print("jumlah : ", str(bagi))
+
+# slide10
+# x = 100
+# y = x-25
+# z = y/5
+# X= x/(z+5)
+# print(X)
+# print(y)
+# print(z)
+
+
+# angka1 = input("Angka 1 :")
+# angka2 = input("Angka 2 :")
+
+# total = int(angka1)+int(angka2)
+# print("Total : ", str(total)) 
+
+A = 10
+B=A
+
+a = 2*A
+print(B)
